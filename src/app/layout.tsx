e25113b5cadbf6b8acf7a5b6c7028545e77d72c3
Sobhan m <veiledassassin99@gmail.com>
2023-05-30 18:00:41 +0430

    replaced <a> with link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import '../../styles/global.css'
 
 export default function DashboardRootLayout({ children }: any) {
@@ -8,9 +9,9 @@ export default function DashboardRootLayout({ children }: any) {
                 <nav className="w-full h-20 fix shadow bg-black text-slate-200
              box-border md:flex md:items-center md:justify-between p-5">
                     <div className="flex justify-between items-center">
-                        <span className="text-2xl font-mono md:ml-0">
+                        <Link className="text-2xl font-mono md:ml-0" href='/'>
                             Sobhan Moadab
-                        </span>
+                        </Link>
                         <span className="mx-2 cursor-pointer md:hidden block">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-9 h-9">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -20,16 +21,24 @@ export default function DashboardRootLayout({ children }: any) {
                     <ul className="font-mono mr-52 md:mr-0 md:flex md:items-center z-[-1] md:z-auto md:static absolute
                 w-full left-0 md:w-auto md:py-0 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500" >
                         <li className="mx-4 my-6 md:my-0 p-1">
-                            <a href="#" className="text-2xl hover:text-cyan-700 duration-500">Articles</a>
+                            <Link className="text-2xl hover:text-cyan-700 duration-500" href='/articles'>
+                                Articles
+                            </Link>
                         </li>
                         <li className="mx-4 my-6 md:my-0 p-1">
-                            <a href="#" className="text-2xl hover:text-cyan-500 duration-500">Projects</a>
+                            <Link className="text-2xl hover:text-cyan-500 duration-500" href='/projects'>
+                                Projects
+                            </Link>
                         </li>
                         <li className="mx-4 my-6 md:my-0 p-1">
-                            <a href="#" className="text-2xl hover:text-cyan-300 duration-500">Github</a>
+                            <Link className="text-2xl hover:text-cyan-300 duration-500" href='https://github.com/SobhanMoadab'>
+                                Github
+                            </Link>
                         </li>
                         <li className="mx-4 my-6 md:my-0 p-1">
-                            <a href="#" className="text-2xl hover:text-cyan-100 duration-500">Contact</a>
+                            <Link className="text-2xl hover:text-cyan-100 duration-500" href='/contact'>
+                                Contact
+                            </Link>
                         </li>
                     </ul>
                 </nav>
